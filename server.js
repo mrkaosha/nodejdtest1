a860d638cd9adb58dc5c2112020264e9c8145589
Joshua Kao <113385802+mrkaosha@users.noreply.github.com>
2022-11-09 12:42:42 -0800

    logs to console

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'text/html' })
   fs.createReadStream('index.html').pipe(res)
+  console.log("connected")
 })
 
 server.listen(process.env.PORT || 3000)
